b13ab1ce87e32d8b8bb57cf5dc2731dbd090293a
wagnert <wagner_tim78@hotmail.com>
2015-01-03 15:46:37 +0100

    Refactoring Github OAuth login

--- src/WEB-INF/classes/Net/Faett/AtChurch/Actions/IndexAction.php
+++ src/WEB-INF/classes/Net/Faett/AtChurch/Actions/IndexAction.php
@@ -23,6 +23,13 @@ namespace Net\Faett\AtChurch\Actions;
 
 use AppserverIo\Psr\Servlet\Http\HttpServletRequest;
 use AppserverIo\Psr\Servlet\Http\HttpServletResponse;
+use AppserverIo\Server\Dictionaries\ServerVars;
+use AppserverIo\Http\HttpProtocol;
+use OAuth\ServiceFactory;
+use OAuth\OAuth2\Service\GitHub;
+use OAuth\Common\Consumer\Credentials;
+use OAuth\Common\Http\Uri\UriFactory;
+use Net\Faett\AtChurch\OAuth\Common\Storage\Session;
 
 /**
  * Default action implementation.
@@ -51,6 +58,144 @@ class IndexAction extends AbstractAction
      */
     public function indexAction(HttpServletRequest $servletRequest, HttpServletResponse $servletResponse)
     {
-        $servletResponse->appendBodyStream('Hello World');
+
+        try {
+
+            // load the github service
+            $gitHub = $this->getGithubService();
+
+            // load the first mail address from the github account
+            $result = json_decode($gitHub->request('user/emails'), true);
+            $servletResponse->appendBodyStream('The first email on your github account is ' . $result[0]);
+
+        } catch (\Exception $e) {
+
+            // log the exception
+            $servletRequest->getContext()->getInitialContext()->getSystemLogger()->error($e->__toString());
+
+            // append a re-login messagge
+            $servletResponse->appendBodyStream('<a href="index.do/index/login">Login with Github!</a>');
+            $servletResponse->setStatusCode(500);
+        }
+    }
+
+    /**
+     * This is the action invokes the Github login.
+     *
+     * @param \AppserverIo\Psr\Servlet\Http\HttpServletRequest  $servletRequest  The request instance
+     * @param \AppserverIo\Psr\Servlet\Http\HttpServletResponse $servletResponse The response instance
+     *
+     * @return void
+     *
+     * @Action(name="/login")
+     */
+    public function loginAction(HttpServletRequest $servletRequest, HttpServletResponse $servletResponse)
+    {
+        try {
+
+            // redirect the the Github authorization URL
+            $servletResponse->setStatusCode(301);
+            $servletResponse->addHeader(HttpProtocol::HEADER_LOCATION, $this->getGithubService()->getAuthorizationUri()->__toString());
+
+        }  catch (\Exception $e) { // if we've a problem, try to re-login
+
+            // log the exception
+            $servletRequest->getContext()->getInitialContext()->getSystemLogger()->error($e->__toString());
+
+            // append a re-login messagge
+            $servletResponse->appendBodyStream('<a href="index.do/index/login">Login with Github!</a>');
+            $servletResponse->setStatusCode(500);
+        }
+    }
+
+    /**
+     * This is a callback action invoked by Github after successfull login.
+     *
+     * @param \AppserverIo\Psr\Servlet\Http\HttpServletRequest  $servletRequest  The request instance
+     * @param \AppserverIo\Psr\Servlet\Http\HttpServletResponse $servletResponse The response instance
+     *
+     * @return void
+     *
+     * @Action(name="/callback")
+     */
+    public function callbackAction(HttpServletRequest $servletRequest, HttpServletResponse $servletResponse)
+    {
+
+        try {
+
+            // query if we've a Github callback code
+            if (($code = $servletRequest->getParameter('code')) == null) {
+                throw new \Exception('Missing "code" parameter in Github callback');
+            }
+
+            // if yes, add it to the session
+            $this->getGithubService()->requestAccessToken($code);
+            $this->indexAction($servletRequest, $servletResponse);
+
+        }  catch (\Exception $e) { // if we've a problem, try to re-login
+
+            // log the exception
+            $servletRequest->getContext()->getInitialContext()->getSystemLogger()->error($e->__toString());
+
+            // append a re-login messagge
+            $servletResponse->appendBodyStream('<a href="index.do/index/login">Login with Github!</a>');
+            $servletResponse->setStatusCode(500);
+        }
+    }
+
+    /**
+     * Returns the URL we want to redirect after the Github login.
+     *
+     * @return string The Github redirect URL
+     */
+    public function getCallbackUrl()
+    {
+
+        // load servlet request
+        $servletRequest = $this->getServletRequest();
+
+        // prepare the URL
+        return sprintf(
+            '%s://%s%s/index/callback',
+            $servletRequest->getServerVar(ServerVars::REQUEST_SCHEME),
+            $servletRequest->getServerVar(ServerVars::SERVER_NAME),
+            $servletRequest->getServerVar(ServerVars::SCRIPT_NAME)
+        );
+    }
+
+    /**
+     * Returns an instance of the Github service we used to load our data.
+     *
+     * @return \OAuth\Common\Service\ServiceInterface The Github service
+     */
+    protected function getGithubService()
+    {
+
+        // se need the actual request instance
+        $servletRequest = $this->getServletRequest();
+
+        // prepare the Github credentials
+        $servicesCredentials = array(
+            'github' => array(
+                'key'       => '442c9adeb30b9aee54d9',
+                'secret'    => 'ee843375d92a773f9afc471a28927ccbf253fade'
+            )
+        );
+
+        // initialize the service factory
+        $serviceFactory = new ServiceFactory();
+
+        // session storage
+        $storage = new Session($servletRequest);
+
+        // Setup the credentials for the requests
+        $credentials = new Credentials(
+            $servicesCredentials['github']['key'],
+            $servicesCredentials['github']['secret'],
+            $callbackUrl = $this->getCallbackUrl()
+        );
+
+        // instantiate the GitHub service using the credentials, http client and storage mechanism for the token
+        return $serviceFactory->createService('GitHub', $credentials, $storage, array('user'));
     }
 }
---
+++ src/WEB-INF/classes/Net/Faett/AtChurch/OAuth/Common/Storage/Session.php
@@ -0,0 +1,218 @@
+<?php
+
+/**
+ * Net\Faett\AtChurch\OAuth\Common\Storage\Session
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * PHP version 5
+ *
+ * @category   Net
+ * @package    Faett
+ * @subpackage AtChurch
+ * @author     Tim Wagner <wagner_tim78@hotmail.com>
+ * @license    http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ * @link       https://github.com/faett-net/at-church
+ */
+
+namespace Net\Faett\AtChurch\OAuth\Common\Storage;
+
+use OAuth\Common\Token\TokenInterface;
+use OAuth\Common\Storage\TokenStorageInterface;
+use OAuth\Common\Storage\Exception\TokenNotFoundException;
+use OAuth\Common\Storage\Exception\AuthorizationStateNotFoundException;
+
+/**
+ * Stores a token in the application servers session.
+ *
+ * @category   Net
+ * @package    Faett
+ * @subpackage AtChurch
+ * @author     Tim Wagner <wagner_tim78@hotmail.com>
+ * @license    http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ * @link       https://github.com/faett-net/at-church
+ */
+class Session implements TokenStorageInterface
+{
+    /**
+     * @var bool
+     */
+    protected $startSession;
+
+    /**
+     * @var string
+     */
+    protected $sessionVariableName;
+
+    /**
+     * @var string
+     */
+    protected $stateVariableName;
+
+    /**
+     * @param \AppserverIo\Psr\Servlet\Http\HttpServletRequest $serlvetRequest The servlet request
+     * @param bool $startSession Whether or not to start the session upon construction.
+     * @param string $sessionVariableName the variable name to use within the _SESSION superglobal
+     * @param string $stateVariableName
+     */
+    public function __construct(
+        $servletRequest,
+        $startSession = true,
+        $sessionVariableName = 'lusitanian_oauth_token',
+        $stateVariableName = 'lusitanian_oauth_state'
+    ) {
+
+        // set the session
+        $this->session = $servletRequest->getSession(true);
+
+        // start the session if not already dony
+        if ($startSession && $this->session->isStarted() === false) {
+            $this->session->start();
+        }
+
+        // initialize the members
+        $this->startSession = $startSession;
+        $this->sessionVariableName = $sessionVariableName;
+        $this->stateVariableName = $stateVariableName;
+
+        // add inital data
+        if ($this->session->getData($sessionVariableName) == null) {
+            $this->session->putData($sessionVariableName, array());
+        }
+        if ($this->session->getData($stateVariableName) == null) {
+            $this->session->putData($stateVariableName, array());
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function retrieveAccessToken($service)
+    {
+        if ($this->hasAccessToken($service)) {
+            $data = $this->session->getData($this->sessionVariableName);
+            return unserialize($data[$service]);
+        }
+        throw new TokenNotFoundException('Token not found in session, are you sure you stored it?');
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function storeAccessToken($service, TokenInterface $token)
+    {
+        $serializedToken = serialize($token);
+        if (is_array($data = $this->session->getData($this->sessionVariableName))) {
+            $data[$service] = $serializedToken;
+            $this->session->putData($this->sessionVariableName, $data);
+        } else {
+            $this->session->putData($this->sessionVariableName, array($service => $serializedToken));
+        }
+        return $this;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function hasAccessToken($service)
+    {
+        if (is_array($data = $this->session->getData($this->sessionVariableName))) {
+            return isset($data[$service]);
+        }
+        return false;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function clearToken($service)
+    {
+        if (is_array($data = $this->session->getData($this->sessionVariableName)) && isset($data[$service])) {
+            unset($data[$service]);
+            $this->session->putData($this->sessionVariableName, $data);
+        }
+        return $this;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function clearAllTokens()
+    {
+        $this->session->putData($this->sessionVariableName, null);
+        return $this;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function storeAuthorizationState($service, $state)
+    {
+        $serializedToken = serialize($token);
+        if (is_array($data = $this->session->getData($this->stateVariableName))) {
+            $data[$service] = $state;
+            $this->session->putData($this->stateVariableName, $data);
+        } else {
+            $this->session->putData($this->stateVariableName, array($service => $state));
+        }
+        return $this;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function hasAuthorizationState($service)
+    {
+        if (is_array($data = $this->session->getData($this->stateVariableName))) {
+            return isset($data[$service]);
+        }
+        return false;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function retrieveAuthorizationState($service)
+    {
+        if ($this->hasAuthorizationState($service)) {
+            $data = $this->session->getData($this->stateVariableName);
+            return $data[$service];
+        }
+        throw new AuthorizationStateNotFoundException('State not found in session, are you sure you stored it?');
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function clearAuthorizationState($service)
+    {
+        if (is_array($data = $this->session->getData($this->stateVariableName)) && isset($data[$service])) {
+            unset($data[$service]);
+            $this->session->putData($this->stateVariableName, $data);
+        }
+        return $this;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function clearAllAuthorizationStates()
+    {
+        $this->session->putData($this->stateVariableName, null);
+        return $this;
+    }
+
+    /**
+     * Default destrcutor implementation.
+     */
+    public function __destruct()
+    {
+        if ($this->startSession) {
+            // save session here
+        }
+    }
+}
